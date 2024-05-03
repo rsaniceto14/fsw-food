f9b7593551cdc42aeddd0235b8d1e361a3374f76
rsaniceto14 <raphaelaniceto14@gmail.com>
2024-05-03 03:10:10 -0300

    feat: add promo-banner on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
+import Image from "next/image";
 
 const Home = () => {
   return (
@@ -13,6 +14,17 @@ const Home = () => {
 
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+      <div className="px-5 pt-6">
+        <Image
+          src="/Banner.png"
+          alt="Até 30% de desconto em pizzas"
+          height={0}
+          width={0}
+          className="h-auto w-full"
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </>
   );
