e100c02f2437c032e104979511238b58e24f6a20
rsaniceto14 <raphaelaniceto14@gmail.com>
2024-05-03 04:59:33 -0300

    feat: add products list and item component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
 import CategoryList from "./_components/category-list";
+import ProductList from "./_components/products-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
 import Image from "next/image";
+import { Button } from "./_components/ui/button";
+import { ChevronRightIcon } from "lucide-react";
+import { db } from "./_lib/prisma";
 
-const Home = () => {
+const Home = async () => {
+  const products = await db.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0, // discount > 0
+      },
+    },
+    take: 10,
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
   return (
     <>
       <Header />
@@ -25,6 +44,20 @@ const Home = () => {
           sizes="100vw"
           quality={100}
         />
+      </div>
+
+      <div className="space-y-4 pt-6">
+        <div className=" flex items-center justify-between px-5">
+          <h2 className="font-semibold">Pedidos Recomendados</h2>
+          <Button
+            variant="ghost"
+            className="h-fit p-0 text-primary hover:bg-transparent"
+          >
+            Ver todos
+            <ChevronRightIcon size={16} />
+          </Button>
+        </div>
+        <ProductList products={products} />
       </div>
     </>
   );
