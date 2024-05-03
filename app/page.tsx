d39f9cbe7b8e4ee84da215a91e9a6589ae4428b1
rsaniceto14 <raphaelaniceto14@gmail.com>
2024-05-03 01:35:15 -0300

    feat: add initial header components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
+import Header from "./_components/header";
+
 const Home = () => {
-
-  return (
-    <div className="bg-black px-0 mx-0">
-      <h1>hello tailwind</h1>
-    </div>
-  );
-
-}
+  return <Header />;
+};
 
 export default Home;
